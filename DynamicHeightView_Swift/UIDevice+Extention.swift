ab7a818c8fb475cc8b855145bce856175b866953
chenqg <chenqg3721@gmail.com>
2020-11-19 11:33:01 +0800

    fix

--- DynamicHeightView_Swift/UIDevice+Extention.swift
+++ DynamicHeightView_Swift/UIDevice+Extention.swift
@@ -12,11 +12,11 @@ public let SAFE_HEIGHT = UIDevice.current.isHair() ? CGFloat(88):CGFloat(64)
 public extension UIDevice {
     
     func isHair() -> Bool {
-        var hasHair:Bool = false
+        var hasHair: Bool = false
         if #available(iOS 11.0, *) {
             var keyWindow = UIApplication.shared.windows.first
             if  keyWindow == nil {
-                keyWindow = UIApplication.shared.windows.first {$0.isKeyWindow}
+                keyWindow = UIApplication.shared.keyWindow
             }
             hasHair = keyWindow?.safeAreaInsets.bottom ?? 0.0 > CGFloat(0.0)
         }
